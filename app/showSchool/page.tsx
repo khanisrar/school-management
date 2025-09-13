feefa52b10727dc4b0bfe09701b80cf4e8bd0297
khanisrar <khan.israr.lap.7651@gmail.com>
2025-09-13 22:22:30 +0530

    dd

--- app/showSchool/page.tsx
+++ app/showSchool/page.tsx
@@ -1,9 +1,10 @@
 // app/showSchool/page.tsx
-import dynamic from "next/dynamic";
+// import dynamic from "next/dynamic";
+import SchoolList from "./schoolList";
 
-const SchoolList = dynamic(() => import("./schoolList"), {
-  ssr: false,
-});
+// const SchoolList = dynamic(() => import("./schoolList"), {
+//   ssr: false,
+// });
 
 export default function ShowSchool() {
   return <SchoolList />;
